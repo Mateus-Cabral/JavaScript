0d0510f3c3dc0a5275f2f2970f75707b9a53365b
Mateus-Cabral <RM93000@fiap.com.br>
2022-04-27 00:49:22 -0300

    Primeira sessão de estudos

---
+++ Comandos iniciais.js
@@ -0,0 +1,38 @@
+//print = console.log(<valor>)
+console.log("Olá");
+
+// Comentário em linha única
+/*
+Comentário em
+várias
+linhas
+*/
+
+console.log("Olá mundo")
+// ; - opcional
+
+{
+    console.log("Bloco 1")
+    {
+        console.log("Bloco 2")
+    }
+}
+// Blocos são criados usando {}
+
+{
+    console.log(1)
+    console.log(3)
+    console.log(5)
+    console.log(7)
+    console.log(9)
+}
+
+{
+    console.log(2)
+    console.log(4)
+    console.log(6)
+    console.log(8)
+    console.log(10)
+}
+
+console.log(Math.sqrt(49))
