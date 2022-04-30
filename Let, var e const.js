efd79a8151186ca4b2c92d97b2804512283bca51
Mateus-Cabral <RM93000@fiap.com.br>
2022-04-30 15:36:23 -0300

    Variáveis

---
+++ Let, var e const.js
@@ -0,0 +1,21 @@
+// Diferenças entre declaração de variáveis
+
+// var declara a variável com escopo global
+var a = 3
+console.log(typeof(a))
+
+
+// let declara a variável com escopo local (usado em funções)
+let b = 4
+
+
+// const declara uma constante (espaço de memória inalterável)
+const pi = 3.14
+
+
+// variáveis podem ser reatribuídas e re-declaradas
+var a = 30
+b = 40
+
+
+console.log(a, b)
