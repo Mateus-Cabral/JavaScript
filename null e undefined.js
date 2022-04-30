bf3503db48c940f60ace8cfabc5d4c2e010ad513
Mateus-Cabral <RM93000@fiap.com.br>
2022-04-30 15:36:45 -0300

    Null e Undefined

---
+++ null e undefined.js
@@ -0,0 +1,7 @@
+// undefined é quando a variável não foi inicializada
+let b
+console.log(b)
+
+// null é quando a variável foi declarada como não tendo nenhum valor primitivo ou endereço
+let a = null
+console.log(a)
